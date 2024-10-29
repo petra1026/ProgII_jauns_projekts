19df9f28a6f6a676ad09ce3c37af67aaf2e47063
petra1026 <149487838+petra1026@users.noreply.github.com>
2024-10-29 12:28:00 +0200

    update

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from dati import pievienot_skolenu
 
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
 def index():
+    if request.method == "POST":
+        vards = request.form['name']
+        uzvards = request.form['lastname']
+        pievienot_skolenu(vards, uzvards)
+        dati = vards+" "+uzvards
+
+        return render_template("index.html", aizsutits = dati)
+    
+    # Get metode
     return render_template("index.html")
+
+@app.route("/pievienot")
+def pievienot():
+    return render_template("pievienot.html")
+
+@app.route("/atzimes")
+def atzimes():
+    return render_template("atzimes.html")
+
 
 
 if __name__ == '__main__':
